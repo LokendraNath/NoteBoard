c174cd6e19f50467633840887e9923d4a2131731
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-29 17:40:00 +0530

    setting up all routes

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,18 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-  res.send("You got 10 notes");
+  res.status(200).send("You got 101 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ msg: "Post Send Successfully" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ msg: "Post Updated Successfully" });
+});
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ msg: "Post Delete Successfully" });
 });
 
 app.listen(5001, () => {
