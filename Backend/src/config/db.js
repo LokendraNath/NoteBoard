83e68e86c467cad78882d4dde861d3a20d24b54d
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-30 12:07:22 +0530

    add database name

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://lokendra-nodeboard-app:nodeboard-lokendra25@cluster0.chos0bq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://lokendra-nodeboard-app:nodeboard-lokendra25@cluster0.chos0bq.mongodb.net/noteboard_db?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("MongoDB Connected Successfully");
   } catch (error) {
