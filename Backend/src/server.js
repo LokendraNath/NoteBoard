5a6641bd0ca7d0560105e9a0326a1e4d9e3a4861
Lokendra Nath <nathdeveloper25@gmail.com>
2025-10-01 19:18:25 +0530

    impliment cors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRouter from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 const PORT = process.env.PORT || 5001;
@@ -11,6 +12,7 @@ const app = express();
 //* Router Middleware
 app.use(express.json());
 app.use(rateLimiter);
+app.use(cors());
 
 app.use("/api/notes", notesRouter);
 
