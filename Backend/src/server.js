3fd83b31adbb869e4029d59a7319e9a9efb63fa9
Lokendra Nath <nathdeveloper25@gmail.com>
2025-10-02 20:28:27 +0530

    setup server.js for deploye

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,24 +1,37 @@
 import express from "express";
-import notesRouter from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
-import rateLimiter from "./middleware/rateLimiter.js";
-dotenv.config();
-const PORT = process.env.PORT || 5001;
+import path from "path";
 
+import notesRouter from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //* Router Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE.ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE.ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontent/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
